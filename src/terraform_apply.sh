181e35b86c563b6e05cd4e55bf9ac78ae3ca2366
Can Elkatmış <canelkatmis@gmail.com>
2019-11-13 00:05:00 +0300

    Update terraform_apply.sh

--- src/terraform_apply.sh
+++ src/terraform_apply.sh
@@ -3,8 +3,9 @@
 function terraformApply {
   # Gather the output of `terraform apply`.
   echo "apply: info: applying Terraform configuration in ${tfWorkingDir}"
-  applyOutput=$(terraform apply -auto-approve -input=false 2>&1)
+  applyOutput=$(terraform apply -auto-approve -input=false ${*} 2>&1)
   applyExitCode=${?}
+  applyCommentStatus="Failed"
 
   # Exit code of 0 indicates success. Print the output and exit.
   if [ ${applyExitCode} -eq 0 ]; then
@@ -12,41 +13,31 @@ function terraformApply {
     echo "${applyOutput}"
     echo
     applyCommentStatus="Success"
-  else
-    # Exit code of !0 indicates failure.
+  fi
+
+  # Exit code of !0 indicates failure.
+  if [ ${applyExitCode} -ne 0 ]; then
     echo "apply: error: failed to apply Terraform configuration in ${tfWorkingDir}"
     echo "${applyOutput}"
     echo
-    applyCommentStatus="Failed"
   fi
 
-
-
-  echo "##### GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME" 
-  echo "##### tfComment: ${tfComment}"
   # Comment on the pull request if necessary.
   if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ]; then
     applyCommentWrapper="#### \`terraform apply\` ${applyCommentStatus}
 <details><summary>Show Output</summary>
-
 \`\`\`
 ${applyOutput}
 \`\`\`
-
 </details>
-+
 *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`*"
 
     applyCommentWrapper=$(stripColors "${applyCommentWrapper}")
     echo "apply: info: creating JSON"
-    applyPayload=$(echo '{}' | jq --arg body "${applyCommentWrapper}" '.body = $body')
+    applyPayload=$(echo "${applyCommentWrapper}" | jq -R --slurp '{body: .}')
     applyCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
     echo "apply: info: commenting on the pull request"
-    #curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${applyPayload}" "${applyCommentsURL}" > /dev/null
-    curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${applyPayload}" "${applyCommentsURL}"
-    echo "##### applyPayload: ${applyPayload}"
-    echo "##### applyCommentsURL: ${applyCommentsURL}"
-    
+    echo "${applyPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${applyCommentsURL}" > /dev/null
   fi
 
   exit ${applyExitCode}
